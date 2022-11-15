07efcac484e791d50950e7521831ed6c67b147f6
WCramRTC <91401050+WCramRTC@users.noreply.github.com>
2022-11-14 18:19:14 -0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture_13_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        } // Main
+
+    } // class
+
+} // namespace
---
+++ README.md
@@ -0,0 +1,2 @@
+# Lecture_13_Arrays
+ Programming 120 - Fall - 2022 - Arrays and Algorithms
